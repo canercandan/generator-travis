30b8919fa0e790c0bd8e41f1c712bd5a5a516ed3
Caner Candan <caner@candan.fr>
2015-11-17 17:27:09 +0100

    * added path option to rebase the destination path of the written/copied files + unit tests

--- generators/app/index.js
+++ generators/app/index.js
@@ -11,18 +11,30 @@ function sortByKeys(a, b) {
 }
 
 module.exports = yeoman.generators.Base.extend({
+  constructor: function () {
+    yeoman.generators.Base.apply(this, arguments);
+
+    // destination path
+    this.option('path', {
+      type: String,
+      required: false,
+      defaults: '',
+      desc: 'Use an other destination path.'
+    });
+  },
+
   writing: {
     app: function () {
       var optional =  this.options.config || {};
-      var existing = this.fs.exists(this.destinationPath('.travis.yml'))
-            ? yaml.parse(this.fs.read(this.destinationPath('.travis.yml')))
+      var existing = this.fs.exists(this.destinationPath(this.options.path + '.travis.yml'))
+            ? yaml.parse(this.fs.read(this.destinationPath(this.options.path + '.travis.yml')))
             : {};
       var defaults = yaml.parse(this.fs.read(this.templatePath('travisyml')));
       var results = mergeAndConcat(existing, optional, defaults);
       var sortedResults = sort(results, { sort: sortByKeys });
       sortedResults.node_js = ramda.uniq(sortedResults.node_js);
       this.fs.write(
-        this.destinationPath('.travis.yml'),
+        this.destinationPath(this.options.path + '.travis.yml'),
         yaml.stringify(sortedResults, 3, 2)
       );
     },
